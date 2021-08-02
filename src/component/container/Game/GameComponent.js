430ac63727830bb37bd183cb6be142f6375126ec
Carlo Rossy <carlorossy88@gmail.com>
2021-08-02 13:35:00 -0400

    added gameplayfunctions

--- src/component/container/Game/GameComponent.js
+++ src/component/container/Game/GameComponent.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import WinnerModalComponent from './WinnerModal/WinnerModalComponent';
-
+import {executeGamePlay} from '../../GameEngine/gameplayFunctions.js'
+import './renderHand.css'
 const GameComponent = () => {
     const [gameOver, setGameOver] = useState(false);
+    
 
     const onPlayAgainHandler = () => {
         setGameOver(false);
@@ -16,6 +18,13 @@ const GameComponent = () => {
             <h1>
                 This is the GamePage
             </h1>
+            <div className="hand">
+              <h1>Play Spades</h1>
+              <button onClick={() => executeGamePlay()}>
+                Click to DeaL
+               </button>
+               <div id="hand"></div>
+            </div>
             <button onClick={() => setGameOver(true)}>
                 Click for game over!
             </button>
